6a0e148eee5f436a35c81cb994aae54213b4ba62
RomiJones <roman.zengsheng@gmail.com>
2022-11-07 12:02:22 +0800

    add SPF and Ethereum address transfer methods

--- utils/MyUtils.js
+++ utils/MyUtils.js
@@ -2,6 +2,9 @@
 let path = require("path");
 const process = require("process");
 const Decimal = require("decimal.js");
+const secp256k1 = require('secp256k1');
+const base58 = require("./base58");
+const {keccak256, sha256} = require('./ethersUtils');
 
 let appendStrToFile = function(theStr, fileName) {
     fs.appendFile(path.join(__dirname, fileName), "\r\n" + theStr, function(err) {
@@ -60,14 +63,58 @@ let isInteger = function(str) {
 	return Decimal.isInteger(str);
 }
 
+const calculationPrefix = "033cf5";
+let transferAddressFromEthToSPF = function(ethAddress) {
+	if(ethAddress.startsWith("0x")) {
+		ethAddress = ethAddress.substring(2);
+	}
+
+	const addressHex = calculationPrefix + ethAddress;
+	const addressBytes = Buffer.from(addressHex, "hex");
+
+	//get checksum
+	let checkSum = doSHA256(doSHA256(addressBytes));
+	checkSum = checkSum.slice(0, 4);
+
+	//将address和checksum组合，然后base58一下得到最终的地址
+	let res = base58.encode58(Buffer.concat([addressBytes, checkSum]));
+	return res;
+}
+
+let transferAddressFromSPFToETH = function(spfAddress) {
+	let temp = base58.decode58(spfAddress);
+	temp = Buffer.from(temp);
+
+	let checkSum = temp.subarray(-4);
+	let addressBytes = temp.subarray(0, temp.length - 4);
+	if(!checkSum.equals(doSHA256(doSHA256(addressBytes)).slice(0, 4))) {
+		return "" //checksum not right
+	}
+
+	let ethAddress = addressBytes.toString("hex");
+	ethAddress = ethAddress.substring(calculationPrefix.length);
+	ethAddress = "0x"+ethAddress;
+	return ethAddress;
+}
+
+
+function doSHA256(msgBytes) {
+	const msgHex = msgBytes.toString("hex");
+	const hashHex = sha256('0x' + msgHex).replace(/^0x/, '')
+	return Buffer.from(hashHex, "hex");
+}
+
 let MyUtils = {
 	appendStrToFile,
 	sleepForMillisecond,
 	getRandomNumbericString,
 	getRandomString,
 	displayCurMemoryUsage,
-	isInteger
+	isInteger,
+	transferAddressFromEthToSPF,
+	transferAddressFromSPFToETH
 }
 
 module.exports = MyUtils;
+
 
